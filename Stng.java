b87c215e8313f5606a5d4810f2707db6fe956c7e
Sumit kumar <sumitkumar808393@gmail.com>
2024-10-13 20:55:18 +0530

    adding string file

---
+++ Stng.java
@@ -0,0 +1,15 @@
+
+
+
+public class Stng {
+    public static void main(String[] args){
+
+        String s1 =  "My name is Sumit";
+        System.out.println(s1);
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.toLowerCase());
+        String txt = "We \t are \f the \b so-called \"Vikings\" from \n the \r north.";
+        System.out.println(txt);
+
+    }
+}
