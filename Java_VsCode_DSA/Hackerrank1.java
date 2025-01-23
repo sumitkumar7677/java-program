cddcbd4399abd3e25380066b1dde57200bc69056
Sumit kumar <sumitkumar808393@gmail.com>
2025-01-23 15:39:53 +0530

    Auto commit on 2025-01-23 15:39:53

---
+++ Java_VsCode_DSA/Hackerrank1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Hackerrank1 {
+
+    public static void main(String[] args) {
+      
+        // Write your code here.
+        
+        Scanner scan = new Scanner(System.in);
+       int i = scan.nextInt();
+       scan.nextLine();
+       double d = scan.nextDouble();
+        scan.nextLine();
+       String s = scan.nextLine();
+
+        
+        System.out.println("Strings: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+       
+       
+        
+        
+    }
+}
