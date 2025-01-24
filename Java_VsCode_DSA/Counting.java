8951d3cec3b97e5792f8e3279e6737ec1b9c9317
Sumit kumar <sumitkumar808393@gmail.com>
2025-01-24 21:09:12 +0530

    Auto commit on 2025-01-24 21:09:12

--- Java_VsCode_DSA/Counting.java
+++ Java_VsCode_DSA/Counting.java
@@ -1,14 +1,4 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 
 
@@ -21,6 +11,7 @@ public class Counting {
     {
         int i;
         for ( i=1 ; i<=10 ; i++){
+            
         System.out.println(N + " x " + i + " = " + N*i);
 
         }
