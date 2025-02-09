7aa4d543d15418581367661ffa23184a61a8e16b
Sumit kumar <sumitkumar808393@gmail.com>
2025-02-09 14:52:15 +0530

    Auto commit on 2025-02-09 14:52:15

--- Java_VsCode_DSA/automateCode.py
+++ Java_VsCode_DSA/automateCode.py
@@ -41,7 +41,7 @@ def auto_commit_and_push():
 # Schedule the task every 5 seconds
 schedule.every(5).seconds.do(auto_commit_and_push)
 
-print("Automation script is running. Press Ctrl+C to stopp.........")
+print("Automation script is running. Press Ctrl+C to stopp..........")
 
 # Keep the script running
 while True:
